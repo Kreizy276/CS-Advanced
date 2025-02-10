4b8b0452f3bca7e7a6d5432f571bc756cb5632c7
Kreizy276 <krisbg02@gmail.com>
2025-02-10 18:59:10 +0200

    classes part

---
+++ DefiningClasses/CarExtension/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsuption;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        public double FuelConsuption
+        {
+            get { return fuelConsuption; }
+            set { fuelConsuption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            if(FuelQuantity - distance * FuelConsuption/100 > 0)
+            {
+                FuelQuantity -= distance * FuelConsuption/100;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:f2}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ DefiningClasses/CarExtension/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Make = "VW",
+                Model = "MK3",
+                Year = 1992,
+                FuelQuantity = 200,
+                FuelConsuption = 200,
+            };
+
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
