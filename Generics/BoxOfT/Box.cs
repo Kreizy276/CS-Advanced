4d36f09fe5bcd537fb937fa4b8d3c310e84f69ff
Kreizy276 <krisbg02@gmail.com>
2025-02-13 18:10:30 +0200

    generics lab

---
+++ Generics/BoxOfT/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> data = new List<T>();
+
+        public int Count { get => data.Count; }
+
+        public void Add(T element)
+        {
+            data.Add(element);
+        }
+
+        public T Remove()
+        {
+            T lastElementToRemove = data.Last();
+            data.RemoveAt(data.Count - 1);
+            return lastElementToRemove;
+        }
+    }
+}
---
+++ Generics/demo/CustomList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    public class CustomList<T>
+    {
+        private List<T> list = new List<T>();
+
+        public List<T> List { get { return list; } }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            list.Remove(item);
+        }
+
+        public string GetAll()
+        {
+            return string.Join(", ", list);
+        }
+    }
+}
